1225d61f1a3df5f287a513bc8b0e6165c417c44a
Peter Quiring <pquiring@gmail.com>
2015-09-15 21:54:10 -0400

    place jf3d project into broken folder

--- broken/jf3d/src/Data.java
+++ broken/jf3d/src/Data.java
@@ -8,7 +8,7 @@ import javaforce.gl.*;
 public class Data {
   public static GLModel model;
   public static GLScene scene;
-  public static GLCanvas canvas;  //holds main OpenGL context (although only visible during startup)
+  public static GLWindow canvas;  //holds main OpenGL context (although only visible during startup)
 
   public static float x,y,z;  //cursor position
 }
