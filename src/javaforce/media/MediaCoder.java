99bb028e4068e8fd054f48bafa116223880e5282
Peter Quiring <pquiring@gmail.com>
2017-01-25 07:43:59 -0500

    don't change http to https

--- src/javaforce/media/MediaCoder.java
+++ src/javaforce/media/MediaCoder.java
@@ -178,7 +178,6 @@ public class MediaCoder {
             return false;
           }
           {
-            url = url.replaceAll("http", "https");
             URL urlAddr = new URL(url);
             URLConnection conn = urlAddr.openConnection();
             conn.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393");
