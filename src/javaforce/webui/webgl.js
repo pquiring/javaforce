5a5fabe9a356e44a166cec1798f88452edcda2f0
Peter Quiring <pquiring@gmail.com>
2017-02-26 09:40:07 -0500

    WebGL : fix Edge compatibility issues

--- src/javaforce/webui/webgl.js
+++ src/javaforce/webui/webgl.js
@@ -1,5 +1,5 @@
 function gl_init(canvas) {
-  canvas.gl = canvas.getContext('webgl');
+  canvas.gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
   var gl = canvas.gl;
   gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
   gl.clearDepth(1.0);                 // Clear everything
