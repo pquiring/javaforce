34b360c898fdae3831262b4e3f4e1c9c62795414
Peter Quiring <pquiring@gmail.com>
2017-06-08 10:26:25 -0400

    jfc : add NOT logic

---
+++ projects/jfcontrols/src/jfcontrols/logic/NOT.java
@@ -0,0 +1,31 @@
+package jfcontrols.logic;
+
+/** Not bit logic.
+ *
+ * @author pquiring
+ */
+
+import javaforce.controls.*;
+
+public class NOT extends Logic {
+
+  public boolean isBlock() {
+    return false;
+  }
+
+  public String getDesc() {
+    return "not";
+  }
+
+  public String getCode(int types[]) {
+    return "enabled = !enabled;\r\n";
+  }
+
+  public int getTagsCount() {
+    return 0;
+  }
+
+  public int getTagType(int idx) {
+    return TagType.unknown;
+  }
+}
