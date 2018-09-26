bc6413c910ffc4035dd87f0bb89079bebbd823c3
Peter Quiring <pquiring@gmail.com>
2018-09-26 10:50:26 -0400

    Package JDK as JRE since Oracle will no longer do so for Java11+

---
+++ jre/readme.txt
@@ -0,0 +1,5 @@
+This is an unoffical Java JRE packaged by JavaForce.
+
+Oracle no longer provides a JRE.
+
+http://javaforce.sourceforge.net
---
+++ jre/src/jre.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+import javax.swing.*;
+
+class jre {
+  public static void main(String args[]) {
+    JOptionPane.showMessageDialog(null, "JRE-11 is packaged by JavaForce\nhttp://javaforce.sourceforge.net", "Notice", JOptionPane.INFORMATION_MESSAGE);
+  }
+};
