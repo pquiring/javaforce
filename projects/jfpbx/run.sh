a522407ff1db22f8facb1cf9c0769e8143b45345
Peter Quiring <pquiring@gmail.com>
2018-09-28 19:01:32 -0400

    fix linux scripts

--- projects/jfpbx/run.sh
+++ projects/jfpbx/run.sh
@@ -3,4 +3,4 @@ if [ $EUID -ne 0 ]; then
   echo Must be root to run this script.
   exit 1
 fi
-java -cp /usr/share/java/javaforce.jar:/usr/share/java/jpbx/bouncycastle.jar:/usr/share/java/jpbx/derby.jar:/usr/share/java/jpbx/jpbxcore.jar jpbx.core.Main $*
+java -cp /usr/share/java/javaforce.jar:/usr/share/java/jpbx/bouncycastle.jar:/usr/share/java/jpbx/derby.jar:/usr/share/java/jpbx/jfpbxcore.jar jpbx.core.Main $*
