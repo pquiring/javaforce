eab40fc7c5f333cfe826b851aebd270751cad9de
Peter Quiring <pquiring@gmail.com>
2019-03-08 15:36:05 -0500

    jfPhone/1.17

--- projects/jfphone/readme.txt
+++ projects/jfphone/readme.txt
@@ -1,4 +1,4 @@
-jPhoneLite/1.16
+jPhoneLite/1.17
 
 Java VoIP Audio/Video SoftPhone (SIP)
 http://jphonelite.sourceforge.net
--- projects/jfphone/src/BasePhone.java
+++ projects/jfphone/src/BasePhone.java
@@ -23,7 +23,7 @@ import javaforce.media.*;
 
 public abstract class BasePhone extends javax.swing.JPanel implements SIPClientInterface, RTPInterface, ActionListener, KeyEventDispatcher {
 
-  public static String version = "1.16";
+  public static String version = "1.17";
 
   public void initBasePhone(GUI gui, WindowController wc) {
     JFLog.init(JF.getUserPath() + "/.jfphone.log", true);
