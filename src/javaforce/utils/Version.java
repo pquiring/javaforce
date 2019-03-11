a2c8dec59f539fab633c41e57a25cd1057029db8
Peter Quiring <pquiring@gmail.com>
2019-03-11 16:55:21 -0400

    jver -> jfver

--- src/javaforce/utils/Version.java
+++ src/javaforce/utils/Version.java
@@ -5,7 +5,7 @@ package javaforce.utils;
  * @author pquiring
  */
 
-public class jver {
+public class Version {
   public static void main(String args[]) {
     System.out.println("jfLinux=" + javaforce.linux.Linux.getVersion());
     System.out.println("JavaForce=" + javaforce.JF.getVersion());
