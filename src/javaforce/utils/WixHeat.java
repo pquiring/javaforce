cdfebd3ad83025482dfc80df7daa139539e4b79e
Peter Quiring <pquiring@gmail.com>
2020-02-16 15:50:53 -0500

    bundle codecs with msi

--- src/javaforce/utils/WixHeat.java
+++ src/javaforce/utils/WixHeat.java
@@ -13,7 +13,7 @@ import java.util.*;
 
 public class WixHeat {
   private static void usage() {
-    System.out.println("Usage:wixheat JAVA_HOME [win32|win64] [--base=folder]");
+    System.out.println("Usage:WixHeat input_folder output_xml base_folder resource_name");
     System.exit(1);
   }
 
@@ -22,11 +22,14 @@ public class WixHeat {
   }
 
   private static StringBuilder out;
-  private static String java_home;
+  private static String input_folder;
+  private static String output_xml;
+  private static String base_folder;
+  private static String res_name;
   private static ArrayList<String> wixfolders = new ArrayList<String>();
   private static ArrayList<Entry> wixfiles = new ArrayList<Entry>();
-  private static boolean win64;
-  private static String base_folder = "jre";
+  private static String dir;
+  private static String cmp;
 
   private static void outHeader() {
     out.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
@@ -35,7 +38,7 @@ public class WixHeat {
 
   private static void addFolder(String parent, String path) throws Exception {
     if (path.startsWith("/")) path = path.substring(1);
-    String fullpath = java_home + path;
+    String fullpath = input_folder + path;
 //    System.out.println("Path:" + fullpath);
     File folder = new File(fullpath);
     File files[] = folder.listFiles();
@@ -62,30 +65,31 @@ public class WixHeat {
   private static void outFolder(String parent, String name, String path) {
     if (name.equals("")) name = base_folder;  //base folder
     String id = path.replaceAll("/", "_");
-    String did = "dir_" + id;
+    String did = dir + id;
     String pid = parent.replaceAll("/", "_");
-    String dpid = "dir_" + pid;
+    String dpid = dir + pid;
     if (parent.equals("APPLICATIONROOTDIRECTORY")) {
       dpid = "APPLICATIONROOTDIRECTORY";
     }
     out.append("<Fragment>\n");
     out.append("  <DirectoryRef Id=\"" + dpid + "\">\n");
-    out.append("    <Directory Id=\"" + did + "\" Name=\"" + name + "\" FileSource=\"" + (java_home + path).replaceAll("/", "\\\\") + "\" />\n");
+    out.append("    <Directory Id=\"" + did + "\" Name=\"" + name + "\" FileSource=\"" + (input_folder + path).replaceAll("/", "\\\\") + "\" />\n");
     out.append("  </DirectoryRef>\n");
     out.append("</Fragment>\n");
   }
 
+  private static int baseguid;
   private static int guid = 1000;
   private static int fileid;
 
   private static void outFiles(String path) {
     String id = path.replaceAll("/", "_");
-    String did = "dir_" + id;
-    String cid = "cmp_" + id;
+    String did = dir + id;
+    String cid = cmp + id;
 
     out.append("<Fragment>\n");
     out.append("    <DirectoryRef Id=\"" + did + "\">\n");
-    out.append("        <Component Id=\"" + cid + "\" Guid=\"{8A8E15CB-3AA6-4D96-AD6D-5241AD6E" + guid++ + "}\"" + (win64 ? " Win64=\"yes\"" : "") + ">\n");
+    out.append("        <Component Id=\"" + cid + "\" Guid=\"{8A8E15CB-3AA6-4D96-AD6D-5241AD" + Integer.toHexString(baseguid) + guid++ + "}\" Win64=\"yes\">\n");
     int cnt = wixfiles.size();
     for(int a=0;a<cnt;a++) {
       Entry e = wixfiles.get(a);
@@ -96,7 +100,7 @@ public class WixHeat {
       } else {
         src = e.path + "/" + e.file;
       }
-      String fid = "_" + fileid++;
+      String fid = dir + "_" + fileid++;
       switch (src) {
         case "bin/java.exe": fid = "java_exe"; break;
         case "bin/javaw.exe": fid = "javaw_exe"; break;
@@ -110,10 +114,10 @@ public class WixHeat {
 
   private static void outTrailer() {
     out.append("<Fragment>\n");
-    out.append("  <ComponentGroup Id=\"JRE\">\n");
+    out.append("  <ComponentGroup Id=\"" + res_name + "\">\n");
     for(int a=0;a<wixfolders.size();a++) {
       String path = wixfolders.get(a).replaceAll("/", "_");
-      out.append("    <ComponentRef Id=\"cmp_" + path + "\" />\n");
+      out.append("    <ComponentRef Id=\"" + cmp + path + "\" />\n");
     }
     out.append("  </ComponentGroup>\n");
     out.append("</Fragment>\n");
@@ -121,28 +125,29 @@ public class WixHeat {
   }
 
   public static void main(String args[]) {
-    if (args.length == 0) {
+    if (args.length != 4) {
       usage();
     }
     //create jvm.xml
     out = new StringBuilder();
-    java_home = args[0].replaceAll("\\\\", "/");
-    for(int a=1;a<args.length;a++) {
-      switch (args[a]) {
-        case "win64": win64 = true; break;
-      }
-      if (args[a].startsWith("--base=")) {
-        base_folder = args[a].substring(7).replaceAll("\\\\", "/");
-        System.out.println("jheat : base=" + base_folder);
-      }
+    input_folder = args[0].replaceAll("\\\\", "/");
+    output_xml = args[1].replaceAll("\\\\", "/");
+    base_folder = args[2].replaceAll("\\\\", "/");
+    res_name = args[3];
+    char ca[] = res_name.toCharArray();
+    for(int a=0;a<ca.length;a++) {
+      baseguid += ca[a];
     }
-    if (!java_home.endsWith("/")) {
-      java_home += "/";
+    baseguid &= 0xff;
+    if (baseguid < 0x10) {
+      baseguid |= 0x10;
     }
+    if (!input_folder.endsWith("/")) {
+      input_folder += "/";
+    }
+    cmp = "cmp_" + res_name + "_";
+    dir = "dir_" + res_name + "_";
     try {
-      File test = new File(java_home + "release");
-      if (!test.exists()) throw new Exception("JAVA_HOME not valid:" + java_home);
-
       outHeader();
 
       addFolder("APPLICATIONROOTDIRECTORY", "");
@@ -153,11 +158,11 @@ public class WixHeat {
 
       outTrailer();
 
-      FileOutputStream fos = new FileOutputStream("jre.xml");
+      FileOutputStream fos = new FileOutputStream(output_xml);
       fos.write(out.toString().getBytes());
       fos.close();
 
-      System.out.println("jheat : jre.xml created");
+      System.out.println("WixHeat : " + output_xml + " created");
 
     } catch (Exception e){
       e.printStackTrace();
