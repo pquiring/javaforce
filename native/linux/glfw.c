7614ca3f90c6c23fd6a19ec11d5683f75d14ea61
Peter Quiring <pquiring@gmail.com>
2020-03-08 13:13:28 -0400

    fix : glfw

--- native/linux/glfw.c
+++ native/linux/glfw.c
@@ -10,6 +10,9 @@
 #include "../glfw/src/input.c"
 #include "../glfw/src/monitor.c"
 #include "../glfw/src/window.c"
+#include "../glfw/src/egl_context.c"
+#include "../glfw/src/vulkan.c"
+#include "../glfw/src/osmesa_context.c"
 
 //linux
 #include "../glfw/src/x11_init.c"
--- native/mac/glfw.m
+++ native/mac/glfw.m
@@ -8,7 +8,9 @@
 #include "../glfw/src/input.c"
 #include "../glfw/src/monitor.c"
 #include "../glfw/src/window.c"
+#include "../glfw/src/egl_context.c"
 #include "../glfw/src/vulkan.c"
+#include "../glfw/src/osmesa_context.c"
 
 //mac
 #include "../glfw/src/cocoa_init.m"
@@ -16,4 +18,5 @@
 #include "../glfw/src/cocoa_time.c"
 #include "../glfw/src/cocoa_window.m"
 #include "../glfw/src/cocoa_joystick.m"
+#include "../glfw/src/posix_thread.c"
 #include "../glfw/src/nsgl_context.m"
