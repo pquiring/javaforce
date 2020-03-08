eaf82c23e4901ff40f59745ad481fdf91010c914
Peter Quiring <pquiring@gmail.com>
2020-03-08 12:51:52 -0400

    mac : fix native build

--- native/mac/glfw.m
+++ native/mac/glfw.m
@@ -14,7 +14,6 @@
 #include "../glfw/src/cocoa_init.m"
 #include "../glfw/src/cocoa_monitor.m"
 #include "../glfw/src/cocoa_time.c"
-#include "../glfw/src/posix_tls.c"
 #include "../glfw/src/cocoa_window.m"
 #include "../glfw/src/cocoa_joystick.m"
 #include "../glfw/src/nsgl_context.m"
