b608b9525f3610172b89224e9fd461266c48e790
Peter Quiring <pquiring@gmail.com>
2020-03-10 11:01:24 -0400

    jfAudio/0.19 - re-release msi for updated libraries

--- projects/jfaudio/src/AudioApp.java
+++ projects/jfaudio/src/AudioApp.java
@@ -12,7 +12,7 @@ import javaforce.media.*;
 
 public class AudioApp extends javax.swing.JFrame implements KeyEventDispatcher {
 
-  public static String version = "0.18";
+  public static String version = "0.19";
 
   /**
    * Creates new form AudioApp
