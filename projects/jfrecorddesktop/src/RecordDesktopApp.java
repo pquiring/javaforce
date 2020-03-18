5dd53484d9273d1e8c6d107826665f86a7fba845
Peter Quiring <pquiring@gmail.com>
2020-03-18 13:00:25 -0400

    jfRecordDesktop/0.9

--- projects/jfrecorddesktop/src/RecordDesktopApp.java
+++ projects/jfrecorddesktop/src/RecordDesktopApp.java
@@ -12,7 +12,7 @@ import javaforce.media.*;
 
 public class RecordDesktopApp extends javax.swing.JFrame {
 
-  private String version = "0.8";
+  private String version = "0.9";
 
   /**
    * Creates new form RecordCameraApp
