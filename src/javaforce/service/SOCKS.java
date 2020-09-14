bbea9930b0843da481945201095837110c960bec
Peter Quiring <pquiring@gmail.com>
2020-09-14 11:47:01 -0400

    add SOCKS5 support

--- src/javaforce/service/SOCKS.java
+++ src/javaforce/service/SOCKS.java
@@ -16,6 +16,8 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
+import javax.net.ssl.*;
+
 import javaforce.*;
 
 public class SOCKS extends Thread {
@@ -25,6 +27,23 @@ public class SOCKS extends Thread {
   private static ArrayList<Session> sessions = new ArrayList<Session>();
   private static Object lock = new Object();
   private static boolean debug = false;
+  private int port;
+  private boolean secure;
+  private static ArrayList<String> user_pass_list = new ArrayList<String>();
+
+  public SOCKS() {
+    port = 1080;
+  }
+
+  public SOCKS(int port, boolean secure) {
+    this.port = port;
+    this.secure = secure;
+  }
+
+  public void addUserPass(String user, String pass) {
+    String user_pass = user + ":" + pass;
+    user_pass_list.add(user_pass);
+  }
 
   public static void addSession(Session sess) {
     synchronized(lock) {
@@ -41,7 +60,11 @@ public class SOCKS extends Thread {
   public void run() {
     JFLog.init(JF.getLogPath() + "/jfsocks.log", true);
     try {
-      ss = new ServerSocket(1080);
+      if (secure) {
+        ss = SSLServerSocketFactory.getDefault().createServerSocket(port);
+      } else {
+        ss = new ServerSocket(port);
+      }
       active = true;
       while (active) {
         Socket s = ss.accept();
@@ -70,11 +93,18 @@ public class SOCKS extends Thread {
     private Socket c;
     private Socket o;
     private ProxyData pd1, pd2;
+    private boolean connected = false;
+    private InputStream cis = null;
+    private OutputStream cos = null;
+    private byte[] req = new byte[1500];
+    private int reqSize = 0;
     public Session(Socket s) {
       c = s;
     }
     public void close() {
-      try { c.close(); } catch (Exception e) {}
+      if (c != null) {
+        try { c.close(); } catch (Exception e) {}
+      }
       if (o != null) {
         try { o.close(); } catch (Exception e) {}
       }
@@ -88,12 +118,6 @@ public class SOCKS extends Thread {
     public void run() {
       //request = 0x04 0x01 port16 ip32 user_id_null [domain_name_null]
       //reply   = 0x00 0x5a reserved[6]   //0x5b = failed
-      byte[] req = new byte[1500];
-      byte[] reply = new byte[8];
-      int reqSize = 0;
-      boolean connected = false;
-      InputStream cis = null;
-      OutputStream cos = null;
       try {
         if (debug) JFLog.log("Session start");
         cis = c.getInputStream();
@@ -103,8 +127,10 @@ public class SOCKS extends Thread {
           int read = cis.read(req, reqSize, 1500 - reqSize);
           if (read < 0) throw new Exception("bad read");
           reqSize += read;
-          if (reqSize > 8) {
-            String ip3 = String.format("%d.%d.%d", req[4], req[5], req[6]);
+          if (reqSize == 0) continue;
+          if (req[0] == 0x04) {
+            if (reqSize < 8) continue;
+            String ip3 = String.format("%d.%d.%d", req[4] & 0xff, req[5] & 0xff, req[6] & 0xff);
             if (ip3.equals("0.0.0")) {
               //domain request
               //look for user_id_null and domain_null
@@ -113,7 +139,7 @@ public class SOCKS extends Thread {
                 if (req[a] == 0) null_count++;
               }
               if (null_count == 2) break;
-              if (null_count > 2) throw new Exception("bad request:too many nulls:expect=2");
+              if (null_count > 2) throw new Exception("SOCKS4:bad request:too many nulls:expect=2");
             } else {
               //ip4 request
               //look for user_id_null
@@ -122,56 +148,160 @@ public class SOCKS extends Thread {
                 if (req[a] == 0) null_count++;
               }
               if (null_count == 1) break;
-              if (null_count > 1) throw new Exception("bad request:too many nulls:expect=1");
+              if (null_count > 1) throw new Exception("SOCKS4:bad request:too many nulls:expect=1");
             }
+          } else if (req[0] == 0x05) {
+            if (reqSize < 3) continue;
+            int nauth = req[1] & 0xff;
+            if (reqSize == nauth + 2) break;
+          } else {
+            throw new Exception("bad request:not SOCKS4/5 request");
           }
         }
-        if (req[0] != 0x04) throw new Exception("bad request:not SOCKS4 request");
-        if (req[1] != 0x01) throw new Exception("bad request:not open socket request");
-        int port = BE.getuint16(req, 2);
-        String user_id;
-        String ip3 = String.format("%d.%d.%d", req[4], req[5], req[6]);
-        if (ip3.equals("0.0.0")) {
-          int user_null = -1;
-          int domain_null = -1;
-          for(int a=8;a<reqSize;a++) {
-            if (req[a] == 0) {
-              if (user_null == -1) {
-                user_null = a;
-              } else {
-                domain_null = a;
-              }
-            }
-          }
-          user_id = new String(req, 8, user_null - 8);
-          String domain = new String(req, 8, domain_null - 8);
-          o = new Socket(domain, port);
-        } else {
-          String ip4 = String.format("%d.%d.%d.%d", req[4], req[5], req[6], req[7]);
-          o = new Socket(ip4, port);
+        switch (req[0]) {
+          case 0x04: socks4(); break;
+          case 0x05: socks5(); break;
+          default: throw new Exception("bad request:not SOCKS4/5 request");
         }
-        connected = true;
-        reply[0] = 0x00;
-        reply[1] = 0x5a;  //success
-        cos.write(reply);
-        //now just proxy data back and forth
-        pd1 = new ProxyData(c,o,"1");
-        pd1.start();
-        pd2 = new ProxyData(o,c,"2");
-        pd2.start();
-        pd1.join();
-        pd2.join();
-        if (debug) JFLog.log("Session end");
       } catch (Exception e) {
         if (debug) JFLog.log(e);
         if (!connected) {
+          byte[] reply = new byte[8];
           reply[0] = 0x00;
           reply[1] = 0x5b;  //failed
           try {cos.write(reply);} catch (Exception e2) {}
         }
-        close();
       }
+      close();
       removeSession(this);
+    }
+
+    private void socks4() throws Exception {
+      if (req[1] != 0x01) throw new Exception("SOCKS4:bad request:not open socket request");
+      int port = BE.getuint16(req, 2);
+      String user_id;  //ignored
+      String ip3 = String.format("%d.%d.%d", req[4] & 0xff, req[5] & 0xff, req[6] & 0xff);
+      if (ip3.equals("0.0.0")) {
+        int user_null = -1;
+        int domain_null = -1;
+        for(int a=8;a<reqSize;a++) {
+          if (req[a] == 0) {
+            if (user_null == -1) {
+              user_null = a;
+            } else {
+              domain_null = a;
+            }
+          }
+        }
+        user_id = new String(req, 8, user_null - 8);
+        String domain = new String(req, 8, domain_null - 8);
+        o = new Socket(domain, port);
+      } else {
+        String ip4 = String.format("%d.%d.%d.%d", req[4] & 0xff, req[5] & 0xff, req[6] & 0xff, req[7] & 0xff);
+        o = new Socket(ip4, port);
+      }
+      connected = true;
+      byte[] reply = new byte[8];
+      reply[0] = 0x00;
+      reply[1] = 0x5a;  //success
+      cos.write(reply);
+      //now just proxy data back and forth
+      pd1 = new ProxyData(c,o,"1");
+      pd1.start();
+      pd2 = new ProxyData(o,c,"2");
+      pd2.start();
+      pd1.join();
+      pd2.join();
+      if (debug) JFLog.log("Session end");
+    }
+
+    private void socks5() throws Exception {
+      //req = 0x05 nauth auth_types[]
+      int nauth = req[1] & 0xff;
+      boolean auth_type_2 = false;
+      for(int a=0;a<nauth;a++) {
+        if (req[a+2] == 0x02) {
+          auth_type_2 = true;
+          break;
+        }
+      }
+      if (!auth_type_2) throw new Exception("SOCKS5:auth not supported");
+      byte[] reply = new byte[2];
+      reply[0] = 0x05;
+      reply[1] = 0x02;
+      cos.write(reply);
+      //read username/password
+      reqSize = 0;
+      while (c.isConnected()) {
+        int read = cis.read(req, reqSize, 1500 - reqSize);
+        if (read < 0) throw new Exception("bad read");
+        reqSize += read;
+        if (reqSize < 5) continue;
+        if (req[0] != 0x01) throw new Exception("SOCKS5:invalid auth request");
+        int user_len = req[1] & 0xff;
+        if (reqSize < 3 + user_len) continue;
+        int pass_len = req[2 + user_len] & 0xff;
+        if (reqSize < 3 + user_len + pass_len) continue;
+        break;
+      }
+      int user_len = req[1] & 0xff;
+      int pass_len = req[2 + user_len] & 0xff;
+      String user = new String(req, 2, user_len);
+      String pass = new String(req, 2 + user_len, pass_len);
+      String user_pass = user + ":" + pass;
+      if (user_pass_list.contains(user_pass)) {
+        throw new Exception("SOCKS5:user/pass not authorized");
+      }
+      reply = new byte[2];
+      reply[0] = 0x01;  //version 1
+      reply[1] = 0x00;  //authorized
+      cos.write(reply);
+      //read connect request
+      reqSize = 0;
+      while (c.isConnected()) {
+        int read = cis.read(req, reqSize, 9 - reqSize);
+        if (read < 0) throw new Exception("bad read");
+        reqSize += read;
+        if (reqSize < 9) continue;
+        int dest_type = req[3];
+        if (dest_type == 0x01) {
+          //ip4
+          if (reqSize == 9) break;
+        } else if (dest_type == 0x03) {
+          //domain name
+          int domain_len = req[4];
+          if (reqSize == 6 + domain_len) break;
+        } else if (dest_type == 0x04) {
+          throw new Exception("SOCKS5:IP6 not supported");
+        } else {
+          throw new Exception("SOCKS5:dest_type not supported:" + dest_type);
+        }
+        break;
+      }
+      if (req[0] != 0x05) throw new Exception("SOCKS5:bad connection request:version != 0x05");
+      if (req[1] != 0x01) throw new Exception("SOCKS5:bad connection request:cmd not supported:" + req[1]);
+      //req[2] = reserved
+      String dest = null;
+      int port = BE.getuint16(req, reqSize - 2);
+      switch (req[3]) {
+        case 0x01: dest = String.format("%d.%d.%d.%d", req[4] & 0xff, req[5] & 0xff, req[6] & 0xff, req[7] & 0xff); break;
+        case 0x03: dest = new String(req, 4, req[4] & 0xff); break;
+        default: throw new Exception("SOCKS5:bad connection request:addr type not supported:" + req[3]);
+      }
+      reply = new byte[reqSize];
+      System.arraycopy(req, 0, reply, 0, reqSize);
+      reply[1] = 0x00;  //success
+      cos.write(reply);
+      o = new Socket(dest, port);
+      connected = true;
+      //now just proxy data back and forth
+      pd1 = new ProxyData(c,o,"1");
+      pd1.start();
+      pd2 = new ProxyData(o,c,"2");
+      pd2.start();
+      pd1.join();
+      pd2.join();
+      if (debug) JFLog.log("Session end");
     }
   }
 
