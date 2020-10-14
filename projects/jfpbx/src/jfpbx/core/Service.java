508c770e5f35a9288986f71c7f0968bb1e5c1baa
Peter Quiring <pquiring@gmail.com>
2020-10-14 13:34:04 -0400

    jfPBX/0.30

--- projects/jfpbx/src/jfpbx/core/Service.java
+++ projects/jfpbx/src/jfpbx/core/Service.java
@@ -17,7 +17,7 @@ import javaforce.jbus.*;
 public class Service implements SIPServerInterface, PBXAPI {
 
   public static String getVersion() {
-    return "0.28";
+    return "0.30";
   }
 
   private SIPServer ss;
