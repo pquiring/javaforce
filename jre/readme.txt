28e118c8263d785376c575783b8ac097d71dfdbe
Peter Quiring <pquiring@gmail.com>
2020-12-08 15:56:48 -0500

    remove jre packager - see AdoptOpenJDK instead

--- jre/readme.txt
+++
@@ -1,5 +0,0 @@
-This is an unoffical Java JRE packaged by JavaForce.
-
-Oracle no longer provides a JRE.
-
-http://javaforce.sourceforge.net
--- jre/src/jre.java
+++
@@ -1,8 +0,0 @@
-import java.awt.*;
-import javax.swing.*;
-
-class jre {
-  public static void main(String args[]) {
-    JOptionPane.showMessageDialog(null, "JRE-11 is packaged by JavaForce\nhttp://javaforce.sourceforge.net", "Notice", JOptionPane.INFORMATION_MESSAGE);
-  }
-};
