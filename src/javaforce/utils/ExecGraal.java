6913580a5eee8508576bca038b8bd4fa0df4cc4f
Peter Quiring <pquiring@gmail.com>
2021-04-12 12:59:09 -0400

    add javaforce.utils.ExecGraal to execute native-image

---
+++ src/javaforce/utils/ExecGraal.java
@@ -0,0 +1,47 @@
+package javaforce.utils;
+
+/** Executes Graal 'native-image' command.
+ *
+ * @author pquiring
+ */
+
+import java.util.*;
+
+import javaforce.*;
+
+public class ExecGraal implements ShellProcessListener {
+  public static void main(String[] args) {
+    if (args.length != 2) {
+      System.out.println("Usage:ExecGraal CLASSPATH MAINCLASS");
+      System.exit(1);
+    }
+    new ExecGraal().run(args);
+  }
+
+  public void run(String[] args) {
+    ShellProcess sp = new ShellProcess();
+    sp.addListener(this);
+    ArrayList<String> cmd = new ArrayList<String>();
+    if (JF.isWindows()) {
+      cmd.add("native-image.cmd");
+    } else {
+      cmd.add("native-image");
+    }
+    cmd.add("-cp");
+    if (JF.isWindows()) {
+      cmd.add(args[0]);  //CLASSPATH
+    } else {
+      cmd.add(args[0].replaceAll("[;]", ":"));  //CLASSPATH
+    }
+    cmd.add(args[1]);  //MAINCLASS
+    cmd.add("-H:JNIConfigurationFiles=graal.json");
+    cmd.add("--shared");
+    cmd.add("--enable-all-security-services");
+    cmd.add("--allow-incomplete-classpath");
+    sp.run(cmd.toArray(new String[0]), true);
+  }
+
+  public void shellProcessOutput(String str) {
+    System.out.print(str);
+  }
+}
