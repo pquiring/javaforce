c441738a1eaa9a77e8c5b33ff4310b47be37674a
Peter Quiring <pquiring@gmail.com>
2022-12-29 18:05:56 -0500

    remove faulty Wikipedia code

--- src/javaforce/gl/Angles3.java
+++ src/javaforce/gl/Angles3.java
@@ -21,26 +21,6 @@ public class Angles3 {
     this.v[2] = z;
   }
 
-  /** Set angles based on Quaternion.
-   * Based on logic from Wikipedia.
-   */
-  public void set(Quaternion q) {
-    // roll (x-axis rotation)
-    float sinr_cosp = 2.0f * (q.w * q.x + q.y * q.z);
-    float cosr_cosp = 1.0f - 2.0f * (q.x * q.x + q.y * q.y);
-    v[0] = (float)(float)Math.atan2(sinr_cosp, cosr_cosp);
-
-    // pitch (y-axis rotation)
-    float sinp = (float)(float)Math.sqrt(1.0f + 2.0f * (q.w * q.x - q.y * q.z));
-    float cosp = (float)(float)Math.sqrt(1.0f - 2.0f * (q.w * q.x - q.y * q.z));
-    v[1] = 2.0f * (float)(float)Math.atan2(sinp, cosp) - (float)(float)Math.PI / 2.0f;
-
-    // yaw (z-axis rotation)
-    float siny_cosp = 2.0f * (q.w * q.z + q.x * q.y);
-    float cosy_cosp = 1.0f - 2.0f * (q.y * q.y + q.z * q.z);
-    v[2] = (float)(float)Math.atan2(siny_cosp, cosy_cosp);
-  }
-
   private float clamp(float value, float min, float max) {
     if (value < min) return min;
     if (value > max) return max;
--- src/javaforce/gl/Quaternion.java
+++ src/javaforce/gl/Quaternion.java
@@ -12,26 +12,6 @@ public class Quaternion {
   public float x,y,z,w;
 
   /** Set values based on Euler Angles (x,y,z). */
-  public void set(float x, float y, float z) {
-    float cr = (float)Math.cos(x * 0.5);
-    float sr = (float)Math.sin(x * 0.5);
-    float cp = (float)Math.cos(y * 0.5);
-    float sp = (float)Math.sin(y * 0.5);
-    float cy = (float)Math.cos(z * 0.5);
-    float sy = (float)Math.sin(z * 0.5);
-
-    this.w = cr * cp * cy + sr * sp * sy;
-    this.x = sr * cp * cy - cr * sp * sy;
-    this.y = cr * sp * cy + sr * cp * sy;
-    this.z = cr * cp * sy - sr * sp * cy;
-  }
-
-  /** Set values based on Euler Angles (Angles3). */
-  public void set(Angles3 a3) {
-    set(a3.v[0], a3.v[1], a3.v[2]);
-  }
-
-  /** Set values based on Euler Angles (x,y,z). */
   public void set(float x, float y, float z, Angles3.Order order) {
     float c1 = (float)Math.cos( x / 2 );
     float c2 = (float)Math.cos( y / 2 );
