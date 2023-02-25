01c8a386c8fae313af938f26d63e426059ceb7bd
Peter Quiring <pquiring@gmail.com>
2023-02-24 21:41:14 -0500

    add packaging for jars in /jars

---
+++ jars/buildAllFedora.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function package {
+  echo Packaging $1
+  cp $1.jar /usr/share/java
+  echo /usr/share/java/$1.jar > files.lst
+  if [ -f build.xml ]; then rm build.xml; fi
+  echo "<project>" >> build.xml
+  echo "<description>$1</description>" >> build.xml
+  echo "<property name=\"app\" value=\"$2\"/>" >> build.xml
+  echo "<property name=\"version\" value=\"$3\"/>" >> build.xml
+  echo "</project>" >> build.xml
+  java -cp javaforce.jar javaforce.utils.GenRPM $2 $3 ..
+  rm files.lst
+  rm build.xml
+}
+
+. ./package-all.sh
---
+++ jars/package-all.sh
@@ -0,0 +1,7 @@
+#called from buildAll*.sh scripts
+
+package jcifs jfcifs 1.3.19
+package jsch jfsch 0.1.55
+package jzlib jfzlib 1.1.3
+package derby jfderby 10.11.1.1
+
