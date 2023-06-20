665f75342f59bfb1246c24f87386ffe06605eb9a
Peter Quiring <pquiring@gmail.com>
2023-06-20 09:22:43 -0400

    cleanup graal config files

--- src/javaforce/utils/ExecGraal.java
+++ src/javaforce/utils/ExecGraal.java
@@ -12,8 +12,6 @@ import javaforce.*;
 
 public class ExecGraal implements ShellProcessListener {
   private static String json_path = "META-INF/native-image";
-  private static boolean use_javaforce_config = true;  //graal needs to be told about these
-  private static boolean use_agent_config = false;  //these should be loaded into the jar file
   public static void main(String[] args) {
     if (args.length != 2) {
       System.out.println("Usage:ExecGraal CLASSPATH MAINCLASS");
@@ -40,44 +38,6 @@ public class ExecGraal implements ShellProcessListener {
     cmd.add(args[1].replaceAll("/", "."));  //MAINCLASS
     cmd.add("--shared");
     cmd.add("--no-fallback");
-
-    //add JF config files
-
-    if (use_javaforce_config) {
-      JFLog.log("Using:META-INF/native-image/javaforce-jni-config.json");
-      cmd.add("-H:JNIConfigurationFiles=" + json_path + "/javaforce-jni-config.json");
-    }
-
-    if (use_javaforce_config) {
-      JFLog.log("Using:META-INF/native-image/javaforce-resource-config.json");
-      cmd.add("-H:ResourceConfigurationFiles=" + json_path + "/javaforce-resource-config.json");
-    }
-
-    //add graal agent config files
-
-    if (use_agent_config) {
-      String json = json_path + "/jni-config.json";
-      if (new File(json).exists()) {
-        JFLog.log("Using:" + json);
-        cmd.add("-H:JNIConfigurationFiles=" + json);
-      }
-    }
-
-    if (use_agent_config) {
-      String json = json_path + "/reflect-config.json";
-      if (new File(json).exists()) {
-        JFLog.log("Using:" + json);
-        cmd.add("-H:ReflectionConfigurationFiles=" + json);
-      }
-    }
-
-    if (use_agent_config) {
-      String json = json_path + "/resource-config.json";
-      if (new File(json).exists()) {
-        JFLog.log("Using:" + json);
-        cmd.add("-H:ResourceConfigurationFiles=" + json);
-      }
-    }
 
     sp.run(cmd.toArray(new String[0]), true);
   }
