16a9c18657fc287c840d11a4fc585b3cc18628ad
Peter Quiring <pquiring@gmail.com>
2023-08-15 20:52:27 -0400

    vss : more cleanup

--- projects/jfbackup/src/vss.cpp
+++ projects/jfbackup/src/vss.cpp
@@ -12,7 +12,6 @@
 #include <vss.h>
 #include <vswriter.h>
 #include <vsbackup.h>
-#include <combaseapi.h>  //StringFromGUID2
 
 #include <cstdio>
 #include <cstdlib>
@@ -60,7 +59,7 @@ char* str16_to_str8(char16* str16, char*str8) {
 }
 
 char* guid_to_string(GUID *guid, char *str) {
-  sprintf(str, "{%02x-%02x-%02x-%02x%02x-%02x%02x%02x%02x%02x%02x}", guid->Data1, guid->Data2, guid->Data3,
+  sprintf(str, "{%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x}", guid->Data1, guid->Data2, guid->Data3,
     guid->Data4[0], guid->Data4[1], guid->Data4[2], guid->Data4[3],
     guid->Data4[4], guid->Data4[5], guid->Data4[6], guid->Data4[7]
   );
