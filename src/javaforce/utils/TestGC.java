de5e9099cc04d3c2379eafb28199349d4330a10f
Peter Quiring <pquiring@gmail.com>
2024-02-24 18:55:52 -0500

    update TestGC - Garbage Collector stress test

--- src/javaforce/utils/TestGC.java
+++ src/javaforce/utils/TestGC.java
@@ -2,15 +2,33 @@ package javaforce.utils;
 
 /** TestGC
  *
+ * Uses 1GB memory with very high turn over, run with max of 8GB.
+ *
+ * GC Options:
+ *
+ * -XX:+UseG1GC                 #default
+ * -XX:+UseZGC -XX:-ZUncommit   #fast
+ * -XX:+UseShenandoahGC         #RedHat only
+ * -XX:+UseSerialGC             #slow
+ * -XX:+UseParallelGC           #slow
+ * -XX:+UseEpsilonGC            #no-op
+ *
+ * Execute:
+ * bin\jfexecd -Xmx8G -XX:+UseZGC -XX:-ZUncommit -cp javaforce.jar javaforce.utils.TestGC
+ *
  * @author pquiring
  */
 
+import java.util.*;
+
+import javaforce.*;
 import javaforce.jni.*;
 
 public class TestGC extends Thread {
-  public static int threadCount = 20;
+  public static int threadCount = 64;
   public static void main(String[] args) {
     TestGC[] threads = new TestGC[threadCount];
+    JF.sleep(3000);
     for(int a=0;a<threadCount;a++) {
       threads[a] = new TestGC();
       threads[a].start();
@@ -22,8 +40,11 @@ public class TestGC extends Thread {
     System.exit(1);
   }
   public void run() {
+    Random r = new Random();
     while (true) {
-      WinNative.hold(new int[1024 * 1024], 10);
+      JF.sleep(50);
+      int size = 16 * 1024 * 1024 + r.nextInt(1024);
+      WinNative.hold(new int[size], 50);
     }
   }
 }
