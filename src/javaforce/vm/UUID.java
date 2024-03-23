01222b2d6102e8af43ee249c286321ffb9b5bc26
Peter Quiring <pquiring@gmail.com>
2024-03-23 09:29:51 -0400

    vm : cleanup UUID generate()

--- src/javaforce/vm/UUID.java
+++ src/javaforce/vm/UUID.java
@@ -13,16 +13,18 @@ public class UUID {
     //format : 36 chars : 8-4-4-4-12
     Random rand = new Random();
     StringBuilder uuid = new StringBuilder();
-    uuid.append(String.format("%08x", rand.nextInt(0x6fffffff) + 0x10000000));
+    uuid.append(String.format("%04x", rand.nextInt(0xffff)));
+    uuid.append(String.format("%04x", rand.nextInt(0xffff)));
     uuid.append('-');
-    uuid.append(String.format("%04x", rand.nextInt(0x6fff) + 0x1000));
+    uuid.append(String.format("%04x", rand.nextInt(0xffff)));
     uuid.append('-');
-    uuid.append(String.format("%04x", rand.nextInt(0x6fff) + 0x1000));
+    uuid.append(String.format("%04x", rand.nextInt(0xffff)));
     uuid.append('-');
-    uuid.append(String.format("%04x", rand.nextInt(0x6fff) + 0x1000));
+    uuid.append(String.format("%04x", rand.nextInt(0xffff)));
     uuid.append('-');
-    uuid.append(String.format("%08x", rand.nextInt(0x6fffffff) + 0x10000000));
-    uuid.append(String.format("%04x", rand.nextInt(0x7fff)));
+    uuid.append(String.format("%04x", rand.nextInt(0xffff)));
+    uuid.append(String.format("%04x", rand.nextInt(0xffff)));
+    uuid.append(String.format("%04x", rand.nextInt(0xffff)));
     if (uuid.length() != 36) {
       System.out.println("UUID.generate() != 36 chars");
       uuid.setLength(36);
