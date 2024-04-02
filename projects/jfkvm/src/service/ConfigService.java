fedefadae1899e3f4ce7a7cd65b15dc92921ea2f
Peter Quiring <pquiring@gmail.com>
2024-04-01 22:41:25 -0400

    vm : NetworkVirtual : add table widths

--- projects/jfkvm/src/service/ConfigService.java
+++ projects/jfkvm/src/service/ConfigService.java
@@ -3458,7 +3458,7 @@ public class ConfigService implements WebUIHandler {
 
       row = new Row();
       tab.add(row);
-      Table table = new Table(new int[] {100, 200, 150, 50}, 20, 6, 0);
+      Table table = new Table(new int[] {100, 200, 150, 50, 100, 50}, 20, 6, 0);
       row.add(table);
       table.setSelectionMode(Table.SELECT_ROW);
       table.setBorder(true);
