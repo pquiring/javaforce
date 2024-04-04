4ef3b2f8e8c41ef7cbeb3b50dafbb6c0fada14a3
Peter Quiring <pquiring@gmail.com>
2024-04-03 20:09:42 -0400

    jfdataloggerplus : secure webui interface

--- projects/jfdataloggerplus/src/App.java
+++ projects/jfdataloggerplus/src/App.java
@@ -3,23 +3,63 @@
  * @author pquiring
  */
 
+import java.io.*;
+
+import javaforce.*;
+import javaforce.service.*;
 import javaforce.webui.*;
 
 public class App implements WebUIHandler {
   public static WebUIServer server;
+  private static KeyMgmt keys;
+  public static WebServerRedir redirService;
 
-  public static void main(String args[]) {
+  private static void initSecureWebKeys() {
+    String dname = "CN=jfkvm.sourceforge.net, O=server, OU=webserver, C=CA";
+    String keyfile = Service.dataPath + "/jfkvm.key";
+    String password = "password";
+    if (new File(keyfile).exists()) {
+      //load existing keys
+      keys = new KeyMgmt();
+      try {
+        FileInputStream fis = new FileInputStream(keyfile);
+        keys.open(fis, password.toCharArray());
+        fis.close();
+      } catch (Exception e) {
+        if (!keys.isValid()) {
+          //generate random keys
+          keys = KeyMgmt.create(keyfile, "webserver", dname, password);
+        }
+        JFLog.log(e);
+      }
+    } else {
+      //generate random keys
+      keys = KeyMgmt.create(keyfile, "webserver", dname, password);
+    }
   }
 
   public static void serviceStart(String args[]) {
     Service.start();
+    initSecureWebKeys();
+    //start config service
     server = new WebUIServer();
-    server.start(new App(), 80);
+    server.start(new App(), 443, keys);
+    //start redir service
+    redirService = new WebServerRedir();
+    redirService.start(80, 443);
   }
 
   public static void serviceStop() {
     Service.stop();
     server.stop();
+    if (redirService != null) {
+      try {
+        redirService.stop();
+      } catch (Exception e) {
+        JFLog.log(e);
+      }
+      redirService = null;
+    }
   }
 
   public Panel getRootPanel(WebUIClient client) {
