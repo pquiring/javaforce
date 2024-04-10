89d0592392154eca2bfa5baed15b2a34669e9b9b
Peter Quiring <pquiring@gmail.com>
2024-04-10 14:16:58 -0400

    jfDVR/0.23

--- projects/jfdvr/src/service/ConfigService.java
+++ projects/jfdvr/src/service/ConfigService.java
@@ -14,7 +14,7 @@ import javaforce.webui.*;
 import javaforce.webui.event.*;
 
 public class ConfigService implements WebUIHandler {
-  public static String version = "0.22";
+  public static String version = "0.23";
   public WebUIServer server;
   private KeyMgmt keys;
   private byte[] cameraicon;
