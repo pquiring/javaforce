c1ccf26c6e4b2a492bcf610406e83d75bfeb01db
Peter Quiring <pquiring@gmail.com>
2024-05-20 09:02:42 -0400

    vm : fix NPE in ConsoleSession

--- projects/jfkvm/src/service/ConsoleSession.java
+++ projects/jfkvm/src/service/ConsoleSession.java
@@ -15,7 +15,7 @@ public class ConsoleSession {
   public VirtualMachine vm;
   public long ts;
 
-  private static HashMap<String, ConsoleSession> sessions;
+  private static HashMap<String, ConsoleSession> sessions = new HashMap<>();
 
   public static ConsoleSession get(String id) {
     return sessions.remove(id);
@@ -25,7 +25,7 @@ public class ConsoleSession {
     sessions.put(id, this);
   }
 
-  private static long _24_hrs = 24 * 60 * 60 * 1000;
+  private static final long _24_hrs = 24 * 60 * 60 * 1000;
 
   public static void clean() {
     long ts = System.currentTimeMillis() - _24_hrs;
