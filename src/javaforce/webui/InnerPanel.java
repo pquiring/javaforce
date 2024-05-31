b29584973b986a4c85df3749ec6f1a272fda1b88
Peter Quiring <pquiring@gmail.com>
2024-05-31 12:46:15 -0400

    webui : improve setAlign on InnerPanel

--- src/javaforce/webui/InnerPanel.java
+++ src/javaforce/webui/InnerPanel.java
@@ -6,23 +6,40 @@ package javaforce.webui;
  */
 
 public class InnerPanel extends Panel {
-  public String header;
-  public InnerPanel(String header) {
-    this.header = header;
+  private static class Legend extends Component {
+    private String text;
+    public Legend(String text) {
+      this.text = text;
+    }
+    public String html() {
+      StringBuilder sb = new StringBuilder();
+      sb.append("<legend");
+      sb.append(getAttrs());
+      sb.append(">");
+      sb.append(text);
+      sb.append("</legend>");
+      return sb.toString();
+    }
+  }
+  private Legend legend;
+  public InnerPanel(String text) {
+    legend = new Legend(text);
     removeClass("panel");
     addClass("innerpanel");
   }
   public String html() {
     StringBuilder sb = new StringBuilder();
     sb.append("<fieldset" + getAttrs() + "'>");
-    if (header != null && header.length() > 0) {
-      sb.append("<legend>" + header + "</legend>");
-    }
+    sb.append(legend.html());
     int cnt = count();
     for(int a=0;a<cnt;a++) {
       sb.append(get(a).html());
     }
     sb.append("</fieldset>");
     return sb.toString();
+  }
+  public void setAlign(int value) {
+    super.setAlign(value);
+    legend.setAlign(value);
   }
 }
