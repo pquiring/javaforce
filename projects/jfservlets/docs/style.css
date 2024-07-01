8d5b1f5077c1476b8bf03b95a317929758279896
Peter Quiring <pquiring@gmail.com>
2024-07-01 16:44:52 -0400

    jfServlets : update help

---
+++ projects/jfservlets/docs/style.css
@@ -0,0 +1,109 @@
+/*
+
+style.css
+
+Author : Peter Quiring
+
+Date : 1/28/2010
+
+*/
+
+body, html {
+  margin: 0px;
+  padding: 0px;
+  background-color: white;
+}
+
+form.thin {
+  margin-top: 0px;
+  margin-bottom: 0px;
+}
+
+table {
+  border-collapse: collapse;
+  width: 33%;
+}
+
+.relative {
+  position: relative;
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.title {
+  position: relative;
+  background: #88ff88;
+  padding: 10px;
+  margin-top: 20px;
+  margin-bottom: 0px;
+  margin-left: 20px;
+  margin-right: 20px;
+  text-align: center;
+  font-weight: bold;
+  vertical-align: bottom;
+  height: 16px;
+  font-size: 10pt;
+  border-radius: 25px;
+}
+
+.title_content_center {
+  background: transparent;
+  text-align: center;
+  font-weight: bold;
+  vertical-align: center;
+  width: 100%;
+  height: 16px;
+  font-size: 10pt;
+}
+
+.title_content_left {
+  padding: 10px;
+  border: 20px;
+  background: transparent;
+  text-align: left;
+  font-weight: bold;
+  vertical-align: center;
+  width: 100%;
+  height: 16px;
+  font-size: 10pt;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+}
+
+.relative {
+  position: relative;
+}
+
+.overlay {
+  position: absolute;
+  top: 0px;
+}
+
+.hidden {
+  display: none;
+}
+
+.content {
+  background: #ddffdd;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-left: 20px;
+  margin-right: 20px;
+//  border: 20px;
+  border-top: 0px;
+  border-bottom: 20px;
+  border-left: 20px;
+  border-right: 20px;
+  border-style: solid;
+  border-color: white;
+  padding: 10px;
+}
+
+img {
+  border: 0px;
+}
+
+h1, h2, h3, h4, ul, li {
+  margin: 0px;
+}
--- projects/jfservlets/readme.txt
+++ projects/jfservlets/readme.txt
@@ -8,12 +8,14 @@ Compatible with:
 
 Not compatible with (and never will be):
  - JSP, Faces, etc. (scripts are for noobs, silly rabbit)
+ - Jakarta
 
 Usage:
  - install jfServlets
  - deploy war files to /deploy folder
    windows : c:\programdata\jfservlets\deploy
    linux : /var/jfservlets/deploy
+ - run jfservlets-keymgr to edit https keys.
 
 API Reference : https://docs.oracle.com/javaee/7/api/index.html
 
