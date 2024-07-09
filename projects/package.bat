227f2f8f1e517571ad96a6bae90149bd1eba4c2f
Peter Quiring <pquiring@gmail.com>
2024-07-09 13:45:58 -0400

    improve projects/package.bat

--- projects/package.bat
+++ projects/package.bat
@@ -1,16 +1,21 @@
 @echo off
 
-if "%1"=="" goto all
+if "%1"=="package" goto package
+if not "%1"=="" goto usage
 
-if not exist %1\wix64.xml goto end
-echo Packaging %1
-echo Packaging %1 1>&2
-cd %1
+for /d %%d in (*) do call package.bat package %%d
+
+:package
+if not exist %2\wix64.xml goto end
+echo Packaging %2
+echo Packaging %2 1>&2
+cd %2
 call ant msi
 cd ..
 goto end
 
-:all
-for /d %%d in (*) do call package.bat %%d
+:usage
+echo This script will package all apps into MSI packages.
+goto end
 
 :end
