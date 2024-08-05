596b2ca62a6f742084f6ecb992a45f3676dfb45c
Peter Quiring <pquiring@gmail.com>
2024-08-05 08:43:41 -0400

    javaforce.utils.SystemInfo : add computer name, serial

--- src/javaforce/utils/SystemInfo.java
+++ src/javaforce/utils/SystemInfo.java
@@ -1,16 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaforce.utils;
 
 import javaforce.awt.*;
 
-/**
+/** SystemInfo
  *
- * @author User
+ * @author peter.quiring
  */
+
+import javaforce.*;
+
 public class SystemInfo extends javax.swing.JFrame {
 
   /**
@@ -21,6 +19,8 @@ public class SystemInfo extends javax.swing.JFrame {
     username.setText(System.getenv("USERNAME"));
     computername.setText(System.getenv("COMPUTERNAME"));
     osversion.setText(System.getProperty("os.name") + " (" + System.getProperty("os.version") + ") " + System.getProperty("os.arch"));
+    system_name.setText(wmic("csproduct", "name"));
+    system_serial.setText(wmic("csproduct", "identifyingnumber"));
     okay.setSelected(true);
     JFAWT.centerWindow(this);
   }
@@ -41,6 +41,10 @@ public class SystemInfo extends javax.swing.JFrame {
     computername = new javax.swing.JTextField();
     jLabel3 = new javax.swing.JLabel();
     osversion = new javax.swing.JTextField();
+    jLabel4 = new javax.swing.JLabel();
+    system_name = new javax.swing.JTextField();
+    jLabel5 = new javax.swing.JLabel();
+    system_serial = new javax.swing.JTextField();
 
     setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
     setTitle("System Info");
@@ -65,6 +69,14 @@ public class SystemInfo extends javax.swing.JFrame {
 
     osversion.setEditable(false);
 
+    jLabel4.setText("System Name");
+
+    system_name.setEditable(false);
+
+    jLabel5.setText("System Serial");
+
+    system_serial.setEditable(false);
+
     javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
     getContentPane().setLayout(layout);
     layout.setHorizontalGroup(
@@ -86,7 +98,15 @@ public class SystemInfo extends javax.swing.JFrame {
           .addGroup(layout.createSequentialGroup()
             .addComponent(jLabel3)
             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-            .addComponent(osversion, javax.swing.GroupLayout.DEFAULT_SIZE, 238, Short.MAX_VALUE)))
+            .addComponent(osversion, javax.swing.GroupLayout.DEFAULT_SIZE, 238, Short.MAX_VALUE))
+          .addGroup(layout.createSequentialGroup()
+            .addComponent(jLabel4)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+            .addComponent(system_name))
+          .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addComponent(jLabel5)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+            .addComponent(system_serial)))
         .addContainerGap())
     );
     layout.setVerticalGroup(
@@ -104,7 +124,15 @@ public class SystemInfo extends javax.swing.JFrame {
         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
           .addComponent(jLabel3)
           .addComponent(osversion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 57, Short.MAX_VALUE)
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+          .addComponent(jLabel4)
+          .addComponent(system_name, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+          .addComponent(jLabel5)
+          .addComponent(system_serial, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         .addComponent(okay)
         .addContainerGap())
     );
@@ -128,13 +156,32 @@ public class SystemInfo extends javax.swing.JFrame {
     });
   }
 
+  private static String wmic(String type, String subtype) {
+    try {
+      //wmic csproduct get vendor,name,identifyingnumber
+      String[] cmd = new String[] {"wmic", type, "get", subtype};
+      ShellProcess sp = new ShellProcess();
+      String output = sp.run(cmd, true);
+      String[] lns = output.split("\r\n");
+      if (lns.length < 2) return "null";
+      return lns[1];
+    } catch (Exception e) {
+      JFLog.log(e);
+    }
+    return null;
+  }
+
   // Variables declaration - do not modify//GEN-BEGIN:variables
   private javax.swing.JTextField computername;
   private javax.swing.JLabel jLabel1;
   private javax.swing.JLabel jLabel2;
   private javax.swing.JLabel jLabel3;
+  private javax.swing.JLabel jLabel4;
+  private javax.swing.JLabel jLabel5;
   private javax.swing.JButton okay;
   private javax.swing.JTextField osversion;
+  private javax.swing.JTextField system_name;
+  private javax.swing.JTextField system_serial;
   private javax.swing.JTextField username;
   // End of variables declaration//GEN-END:variables
 }
