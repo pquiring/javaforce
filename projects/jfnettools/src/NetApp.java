b78645f2a481c092dad8db315a531c0c1added70
Peter Quiring <pquiring@gmail.com>
2025-02-13 11:00:30 -0500

    jfnettools : Wifi Analyzer : add Auth column

--- projects/jfnettools/src/NetApp.java
+++ projects/jfnettools/src/NetApp.java
@@ -793,15 +793,20 @@ public class NetApp extends javax.swing.JFrame implements WifiAnalyzer.Callback 
 
     wifi_table.setModel(new javax.swing.table.DefaultTableModel(
       new Object [][] {
-        {null, null, null, null},
-        {null, null, null, null},
-        {null, null, null, null},
-        {null, null, null, null}
+
       },
       new String [] {
-        "SSID", "Freq", "Signal", "Type"
+        "SSID", "Freq", "Signal", "Authentication", "Encryption"
       }
-    ));
+    ) {
+      Class[] types = new Class [] {
+        java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+      };
+
+      public Class getColumnClass(int columnIndex) {
+        return types [columnIndex];
+      }
+    });
     jScrollPane4.setViewportView(wifi_table);
 
     wifi_config.setText("Config");
@@ -1560,7 +1565,7 @@ public class NetApp extends javax.swing.JFrame implements WifiAnalyzer.Callback 
         }
         for(SSID ssid : ssids) {
           for(SSID.AP ap : ssid.aps) {
-            model.addRow(new String[] {ssid.ssid, Integer.toString(ap.channel), Integer.toString(ap.dbm), ssid.encryption});
+            model.addRow(new String[] {ssid.ssid, Integer.toString(ap.channel), Integer.toString(ap.dbm), ssid.auth, ssid.encryption});
           }
         }
         int signal = getSpotSignalStrength();
