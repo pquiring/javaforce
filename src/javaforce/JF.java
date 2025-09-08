115d2a2dc8565e36df329922a48c07f07a64520c
Peter Quiring <pquiring@gmail.com>
2025-09-08 08:24:45 -0400

    JF/87.0

--- src/javaforce/JF.java
+++ src/javaforce/JF.java
@@ -21,7 +21,7 @@ import java.security.cert.Certificate;
 public class JF {
 
   public static String getVersion() {
-    return "86.2";
+    return "87.0";
   }
 
   public static void main(String[] args) {
