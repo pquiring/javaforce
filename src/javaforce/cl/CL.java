0a8a6cdf4342565902b1de5edcc4daf82d8018a5
Peter Quiring <pquiring@gmail.com>
2026-01-21 08:54:56 -0500

    move CL test to javaforce.tests.TestCL and document CL

--- src/javaforce/cl/CL.java
+++ src/javaforce/cl/CL.java
@@ -5,10 +5,6 @@ package javaforce.cl;
  * @author peter.quiring
  */
 
-import java.io.*;
-import java.util.*;
-
-import javaforce.*;
 import javaforce.jni.*;
 
 public interface CL {
@@ -22,129 +18,71 @@ public interface CL {
     return CLJNI.getInstance();
   }
 
+  /** Create OpenGL Context.
+   * close() context to release it.
+   */
   public long create(String src, int type);
+
+  /** Create OpenGL Context (type = TYPE_DEFAULT).
+   * close() context to release it.
+   */
   public long create(String src);
 
+  /** Create OpenGL Kernel from source code.
+   * freeKernel() to release it.
+   */
   public long kernel(long ctx, String func);
 
   public static final int MEM_READ_WRITE = (1 << 0);
   public static final int MEM_WRITE = (1 << 1);
   public static final int MEM_READ = (1 << 2);
 
+  /** Create a read-only buffer. */
   public long createReadBuffer(long ctx, int size);
+  /** Create a write-only buffer. */
   public long createWriteBuffer(long ctx, int size);
+  /** Create a read-write buffer. */
   public long createReadWriteBuffer(long ctx, int size);
 
+  /** Set a kernel parameter. */
   public boolean setArg(long ctx, long kernel, int idx, byte[] value);
 
+  /** Set a kernel parameter. */
   public boolean setArg(long ctx, long kernel, int idx, int value);
 
+  /** Set a kernel parameter. */
   public boolean setArg(long ctx, long kernel, int idx, long value);
 
+  /** Write to a buffer. */
   public boolean writeBuffer(long ctx, long buffer, byte[] data);
 
+  /** Write to a buffer. */
   public boolean writeBuffer(long ctx, long buffer, float[] data);
 
+  /** Execute a kernel with one counter. */
   public boolean execute(long ctx, long kernel, int count);
 
+  /** Execute a kernel with two counters. */
   public boolean execute2(long ctx, long kernel, int count1, int count2);
 
+  /** Execute a kernel with three counters. */
   public boolean execute3(long ctx, long kernel, int count1, int count2, int count3);
 
+  /** Execute a kernel with four counters. */
   public boolean execute4(long ctx, long kernel, int count1, int count2, int count3, int count4);
 
+  /** Read from a buffer. */
   public boolean readBuffer(long ctx, long buffer, byte[] data);
 
+  /** Read from a buffer. */
   public boolean readBuffer(long ctx, long buffer, float[] data);
 
+  /** Release a kernel. */
   public boolean freeKernel(long ctx, long kernel);
 
+  /** Release a buffer. */
   public boolean freeBuffer(long ctx, long buffer);
 
+  /** Release an OpenCL context. */
   public boolean close(long ctx);
-
-  public static void main(String[] args) {
-    Compute compute = new Compute();
-    if (!compute.init(TYPE_GPU)) {
-      JFLog.log("Compute init failed");
-      System.exit(1);
-    }
-    int SIZE;
-    int SIZE_SIZE;
-    Random rand = new Random();
-    System.out.println("Starting tests...");
-    //array_square
-    try {
-      SIZE = 16;
-      float[] a = new float[SIZE];
-      for(int i=0;i<SIZE;i++) {
-        a[i] = rand.nextFloat();
-      }
-      float[] b = new float[SIZE];
-
-      compute.array_square(a, b);
-
-      //confirm results
-      int correct = 0;
-      for(int i=0;i<SIZE;i++) {
-        float res = a[i] * a[i];
-        if (b[i] == res) {
-          correct++;
-        } else {
-          JFLog.log("error:b[]=" + b[i] + ",expected=" + res);
-        }
-      }
-
-      System.out.println("array_square:" + correct + " of " + SIZE + " are correct");
-    } catch (Throwable t) {
-      JFLog.log(t);
-    }
-    //matrix_mult
-    try {
-      boolean identity = true;
-      SIZE = 3;  //3*3 = 9
-      SIZE_SIZE = SIZE * SIZE;
-      float[] a = new float[SIZE_SIZE];
-      float[] b = new float[SIZE_SIZE];
-      float[] c = new float[SIZE_SIZE];
-      int idx = 0;
-      for(int row=0;row<SIZE;row++) {
-        for(int col=0;col<SIZE;col++) {
-          a[idx] = rand.nextFloat();
-          if (identity) {
-            b[idx] = (row == col ? 1 : 0);  //identity matrix
-          } else {
-            b[idx] = rand.nextFloat();
-          }
-          idx++;
-        }
-      }
-      compute.matrix_mult(SIZE, SIZE, SIZE, a, b, c);
-
-      //confirm results
-      int correct = 0;
-      for(int row=0;row<SIZE;row++) {
-        for(int col=0;col<SIZE;col++) {
-          int i = col * SIZE + row;
-          float res = 0;
-          for(int k=0;k<SIZE;k++) {
-            res += a[k * SIZE + row] * b[col * SIZE + k];
-          }
-          if (c[i] == res) {
-            correct++;
-          } else {
-            JFLog.log("error:c[] = " + c[i] + ":expected=" + res);
-          }
-        }
-      }
-      if (true) {
-        javaforce.Console.printArray(a);
-        javaforce.Console.printArray(b);
-        javaforce.Console.printArray(c);
-      }
-      System.out.println("matrix_mult:" + correct + " of " + SIZE_SIZE + " are correct");
-    } catch (Throwable t) {
-      JFLog.log(t);
-    }
-  }
 }
---
+++ src/javaforce/tests/TestCL.java
@@ -0,0 +1,99 @@
+package javaforce.tests;
+
+/** Test OpenCL
+ *
+ * @author pquiring
+ */
+
+import java.util.*;
+
+import javaforce.*;
+import javaforce.cl.*;
+import static javaforce.cl.CL.*;
+
+public class TestCL {
+  public static void main(String[] args) {
+    Compute compute = new Compute();
+    if (!compute.init(TYPE_GPU)) {
+      JFLog.log("Compute init failed");
+      System.exit(1);
+    }
+    int SIZE;
+    int SIZE_SIZE;
+    Random rand = new Random();
+    System.out.println("Starting tests...");
+    //array_square
+    try {
+      SIZE = 16;
+      float[] a = new float[SIZE];
+      for(int i=0;i<SIZE;i++) {
+        a[i] = rand.nextFloat();
+      }
+      float[] b = new float[SIZE];
+
+      compute.array_square(a, b);
+
+      //confirm results
+      int correct = 0;
+      for(int i=0;i<SIZE;i++) {
+        float res = a[i] * a[i];
+        if (b[i] == res) {
+          correct++;
+        } else {
+          JFLog.log("error:b[]=" + b[i] + ",expected=" + res);
+        }
+      }
+
+      System.out.println("array_square:" + correct + " of " + SIZE + " are correct");
+    } catch (Throwable t) {
+      JFLog.log(t);
+    }
+    //matrix_mult
+    try {
+      boolean identity = true;
+      SIZE = 3;  //3*3 = 9
+      SIZE_SIZE = SIZE * SIZE;
+      float[] a = new float[SIZE_SIZE];
+      float[] b = new float[SIZE_SIZE];
+      float[] c = new float[SIZE_SIZE];
+      int idx = 0;
+      for(int row=0;row<SIZE;row++) {
+        for(int col=0;col<SIZE;col++) {
+          a[idx] = rand.nextFloat();
+          if (identity) {
+            b[idx] = (row == col ? 1 : 0);  //identity matrix
+          } else {
+            b[idx] = rand.nextFloat();
+          }
+          idx++;
+        }
+      }
+      compute.matrix_mult(SIZE, SIZE, SIZE, a, b, c);
+
+      //confirm results
+      int correct = 0;
+      for(int row=0;row<SIZE;row++) {
+        for(int col=0;col<SIZE;col++) {
+          int i = col * SIZE + row;
+          float res = 0;
+          for(int k=0;k<SIZE;k++) {
+            res += a[k * SIZE + row] * b[col * SIZE + k];
+          }
+          if (c[i] == res) {
+            correct++;
+          } else {
+            JFLog.log("error:c[] = " + c[i] + ":expected=" + res);
+          }
+        }
+      }
+      if (true) {
+        javaforce.Console.printArray(a);
+        javaforce.Console.printArray(b);
+        javaforce.Console.printArray(c);
+      }
+      System.out.println("matrix_mult:" + correct + " of " + SIZE_SIZE + " are correct");
+    } catch (Throwable t) {
+      JFLog.log(t);
+    }
+  }
+}
