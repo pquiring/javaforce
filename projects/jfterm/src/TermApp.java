87d64ee876a59c155770c329d2251cc1bf58ebb8
Peter Quiring <pquiring@gmail.com>
2026-01-23 13:04:22 -0500

    jfTerm/0.27

--- projects/jfterm/src/TermApp.java
+++ projects/jfterm/src/TermApp.java
@@ -21,7 +21,7 @@ import javaforce.ansi.client.*;
 
 public class TermApp extends javax.swing.JFrame implements KeyEventDispatcher {
 
-  public static String version = "0.26";
+  public static String version = "0.27";
 
   public boolean dispatchKeyEvent(KeyEvent e) {
     //System.out.println("KeyEvent:" + e);
