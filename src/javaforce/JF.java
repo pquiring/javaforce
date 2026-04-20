c49bd37fd2eb536803a2fe3955b581eec175d7da
Peter Quiring <pquiring@gmail.com>
2026-04-20 09:15:07 -0400

    JF/104.1

--- src/javaforce/JF.java
+++ src/javaforce/JF.java
@@ -27,7 +27,7 @@ public class JF {
 
   /** Returns JavaForce version as a String. */
   public static String getVersion() {
-    return "104.0";
+    return "104.1";
   }
 
   /** Console app that displays JavaForce version. */
