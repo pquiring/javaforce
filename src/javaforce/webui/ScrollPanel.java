3274566100c2a30705168b6717c7baadbe909609
Peter Quiring <pquiring@gmail.com>
2026-06-17 18:28:42 -0400

    webui : add ScrollPanel.setScroll()

--- src/javaforce/webui/ScrollPanel.java
+++ src/javaforce/webui/ScrollPanel.java
@@ -6,9 +6,20 @@ package javaforce.webui;
  */
 
 public class ScrollPanel extends Panel {
+  private boolean scroll = true;
   public ScrollPanel() {
     setFlexDirection(NONE);
     setOverflow(AUTO);
     addClass("flex");
+  }
+  /** Set scroll state. */
+  public void setScroll(boolean scroll) {
+    if (this.scroll == scroll) return;
+    if (scroll) {
+      setOverflow(AUTO);
+    } else {
+      setOverflow(HIDDEN);
+    }
+    this.scroll = scroll;
   }
 }
