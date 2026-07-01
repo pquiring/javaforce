638588dff0fff58a63045dcc812962addf8bddf8
Peter Quiring <pquiring@gmail.com>
2026-07-01 08:27:22 -0400

    add javaforce.PackageInfo

---
+++ src/javaforce/PackageInfo.java
@@ -0,0 +1,12 @@
+package javaforce;
+
+import java.lang.annotation.*;
+
+/** PackageInfo Annotation
+ *
+ * @author pquiring
+ */
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.PACKAGE)
+public @interface PackageInfo {}
