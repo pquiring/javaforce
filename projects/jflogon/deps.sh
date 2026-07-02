9437b337280feebd3d0dd489b613dff38cb0ef2c
Peter Quiring <pquiring@gmail.com>
2026-07-01 22:06:01 -0400

    add jflogon deps.sh

---
+++ projects/jflogon/deps.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# install linux deps for running app
+
+function detectos {
+  if [ ! -f /etc/os-release ]; then
+    echo Unable to detect os
+    echo /etc/os-release not found!
+    ID=unknown
+  fi
+  . /etc/os-release
+  #remove "quotes"
+  ID=${ID//\"/}
+  VERSION_ID=${VERSION_ID//\"/}
+  case $ID in
+    debian | ubuntu)
+      OS=debian
+      ;;
+    fedora | centos)
+      OS=fedora
+      ;;
+    arch)
+      OS=arch
+      ;;
+    freebsd)
+      OS=freebsd
+      ;;
+    *)
+      OS=unknown
+      echo Unknown os detected!
+      echo ID=$ID
+      ;;
+  esac
+}
+
+#check if root
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root"
+  exit 1
+fi
+
+function debian {
+  apt update
+  xargs -d '\n' apt --yes install < deb.deps
+}
+
+function fedora {
+  xargs -d '\n' dnf -y install < rpm.deps
+}
+
+function arch {
+  pacman -Sy
+  xargs -d '\n' pacman -S --noconfirm < pac.deps
+}
+
+function freebsd {
+  pkg install -y git ant
+}
+
+detectos
+
+case $OS in
+  debian)
+    debian
+    ;;
+  fedora)
+    fedora
+    ;;
+  arch)
+    arch
+    ;;
+  freebsd)
+    freebsd
+    ;;
+  *)
+    echo OS installation not available yet!
+    echo OS=$OS
+    exit
+    ;;
+esac
+
+echo Deps install complete!
