5691c1b9a49186c3640e020a52907763a5d9ac5f
Peter Quiring <pquiring@gmail.com>
2026-07-28 12:17:07 -0400

    jfCommand : add taskschd

--- projects/jfcommand/src/CommandApp.java
+++ projects/jfcommand/src/CommandApp.java
@@ -88,6 +88,7 @@ public class CommandApp implements ActionListener {
       cfg.append("eventvwr=mmc.exe eventvwr.msc\n");
       cfg.append("certmgr=mmc.exe certmgr.msc\n");
       cfg.append("compmgmt=mmc.exe compmgmt.msc\n");
+      cfg.append("taskschd=mmc.exe taskschd.msc\n");
       cfg.append("virtmgmt=mmc.exe virtmgmt.msc\n");
       cfg.append("sysdm=rundll32.exe shell32.dll,Control_RunDLL sysdm.cpl\n");
       cfg.append("timedate=rundll32.exe shell32.dll,Control_RunDLL timedate.cpl\n");
