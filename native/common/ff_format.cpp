8150f9fb4a2537a2cfee558a91a23ea0f3adc9c9
Peter Quiring <pquiring@gmail.com>
2026-09-01 19:13:47 -0400

    ffmpeg : add NULL checks

--- native/common/ff_format.cpp
+++ native/common/ff_format.cpp
@@ -3,31 +3,36 @@
 jint getVideoStream(FFContext *ctx)
 {
   if (ctx == NULL) return -1;
+  if (ctx->video_stream == NULL) return -1;
   return ctx->video_stream->index;
 }
 
 jint getAudioStream(FFContext *ctx)
 {
   if (ctx == NULL) return -1;
+  if (ctx->audio_stream == NULL) return -1;
   return ctx->audio_stream->index;
 }
 
 jint getVideoCodecID(FFContext *ctx)
 {
   if (ctx == NULL) return -1;
+  if (ctx->video_stream == NULL) return -1;
+  if (ctx->video_stream->codecpar == NULL) return -1;
   return ctx->video_stream->codecpar->codec_id;
 }
 
 jint getAudioCodecID(FFContext *ctx)
 {
   if (ctx == NULL) return -1;
+  if (ctx->audio_stream == NULL) return -1;
+  if (ctx->audio_stream->codecpar == NULL) return -1;
   return ctx->audio_stream->codecpar->codec_id;
 }
 
 jint getVideoBitRate(FFContext *ctx)
 {
   if (ctx == NULL) return 0;
-
   if (ctx->video_codec_ctx == NULL) return 0;
   return ctx->video_codec_ctx->bit_rate;
 }
@@ -35,7 +40,6 @@ jint getVideoBitRate(FFContext *ctx)
 jint getAudioBitRate(FFContext *ctx)
 {
   if (ctx == NULL) return 0;
-
   if (ctx->audio_codec_ctx == NULL) return 0;
   return ctx->audio_codec_ctx->bit_rate;
 }
