a3e44cf736f969ac7984eb2e521a52ce2ff16884
Peter Quiring <pquiring@gmail.com>
2026-09-08 12:30:50 -0400

    jfDesktop : Session : log environment

--- projects/jfdesktop/src/Session.java
+++ projects/jfdesktop/src/Session.java
@@ -9,6 +9,7 @@ public class Session {
   private static int LOG_DEFAULT = 0;
   public static void main(String[] args) {
     JFLog.init(LOG_DEFAULT, JF.getUserPath() + "/.jfdesktop-session.log", true);
+    log_env();
     try {
       java.awt.EventQueue.invokeLater(new Runnable() {
         public void run() {
@@ -24,6 +25,13 @@ public class Session {
       });
     } catch (Throwable t) {
       JFLog.log(t);
+    }
+  }
+  private static void log_env() {
+    JFLog.log(LOG_DEFAULT, "Environment:");
+    String[] envs = JF.getEnvironment();
+    for(String e : envs) {
+      JFLog.log(LOG_DEFAULT, e);
     }
   }
 }
