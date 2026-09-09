e21b985094c9989146329e5830a485b4b75739c8
Peter Quiring <pquiring@gmail.com>
2026-09-09 11:50:38 -0400

    cleaup linux/install-jbrsdk.sh

--- linux/install-jbrsdk.sh
+++ linux/install-jbrsdk.sh
@@ -7,7 +7,7 @@ cd /usr/lib/jvm
 
 wget https://cache-redirector.jetbrains.com/intellij-jbr/$FILE
 tar xf $FILE
-mv jbrsdk-25.0.4.1-linux-x64-b583.48 jbrsdk-$VERSION-amd64
+mv jbrsdk-25.0.4.1-linux-x64-b583.48 jbrsdk-$VERSION-openjdk-amd64
 
 wget http://pquiring.github.io/javaforce/linux/jbrsdk-$VERSION-openjdk-amd64.jinfo
 mv jbrsdk-$VERSION-openjdk-amd64.jinfo .jbrsdk-$VERSION-openjdk-amd64.jinfo
