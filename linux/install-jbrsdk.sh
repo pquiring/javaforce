ab83eb1a361a4b170fdc674fac04eaa02efd0e92
Peter Quiring <pquiring@gmail.com>
2026-09-09 14:20:37 -0400

    install-jbrsdk : install more execs

--- linux/install-jbrsdk.sh
+++ linux/install-jbrsdk.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+function install() {
+  update-alternatives --install /usr/bin/$1 $1 /usr/lib/jvm/jbrsdk-$VERSION-openjdk-amd64/bin/$1 2540
+  update-alternatives --set $1 /usr/lib/jvm/jbrsdk-$VERSION-openjdk-amd64/bin/$1
+}
+
 VERSION=25.0.4
 FILE=jbrsdk-25.0.4.1-linux-x64-b583.48.tar.gz
 
@@ -15,5 +20,9 @@ mv jbrsdk-25.0.4.1-linux-x64-b583.48 jbrsdk-$VERSION-openjdk-amd64
 #update-java-alternatives -s jbrsdk-$VERSION-openjdk-amd64
 
 #install manually
-update-alternatives --install /usr/bin/java java /usr/lib/jvm/jbrsdk-$VERSION-openjdk-amd64/bin/java 2540
-update-alternatives --set java /usr/lib/jvm/jbrsdk-$VERSION-openjdk-amd64/bin/java
+install java
+install javac
+install javadoc
+install jlink
+install jimage
+install keytool
