33c1b187929beba4815cdaf06503d6e62f8c2680
Peter Quiring <pquiring@gmail.com>
2026-09-09 10:22:05 -0400

    jfMedia/0.32

--- projects/jfmedia/src/MediaApp.java
+++ projects/jfmedia/src/MediaApp.java
@@ -17,7 +17,7 @@ import javaforce.voip.*;
 
 public class MediaApp extends javax.swing.JFrame {
 
-  public static String version = "0.31";
+  public static String version = "0.32";
 
   private static String logfile = JF.getUserPath() + "/.jfmedia.log";
 
