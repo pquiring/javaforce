228a5a18f3d991fea5432b908e074a310d187722
Peter Quiring <pquiring@gmail.com>
2026-09-09 12:09:13 -0400

    install-jbrsdk.sh : use update-alternatives

--- linux/install-jbrsdk.sh
+++ linux/install-jbrsdk.sh
@@ -9,7 +9,11 @@ wget https://cache-redirector.jetbrains.com/intellij-jbr/$FILE
 tar xf $FILE
 mv jbrsdk-25.0.4.1-linux-x64-b583.48 jbrsdk-$VERSION-openjdk-amd64
 
-wget http://pquiring.github.io/javaforce/linux/jbrsdk-$VERSION-openjdk-amd64.jinfo
-mv jbrsdk-$VERSION-openjdk-amd64.jinfo .jbrsdk-$VERSION-openjdk-amd64.jinfo
+#install with update-java-alternatives (not working)
+#wget http://pquiring.github.io/javaforce/linux/jbrsdk-$VERSION-openjdk-amd64.jinfo
+#mv jbrsdk-$VERSION-openjdk-amd64.jinfo .jbrsdk-$VERSION-openjdk-amd64.jinfo
+#update-java-alternatives -s jbrsdk-$VERSION-openjdk-amd64
 
-update-java-alternatives -s jbrsdk-$VERSION-openjdk-amd64
+#install manually
+update-alternatives --install /usr/bin/java java /usr/lib/jvm/jbrsdk-$VERSION-openjdk-amd64/bin/java 2540
+update-alternatives --set java /usr/lib/jvm/jbrsdk-$VERSION-openjdk-amd64/bin/java
