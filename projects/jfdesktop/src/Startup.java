af73b58edb305b0ac9003339d59ce65533f51c84
Peter Quiring <pquiring@gmail.com>
2026-09-10 15:06:54 -0400

    jfDesktop : change default taskbar height

--- projects/jfdesktop/src/Startup.java
+++ projects/jfdesktop/src/Startup.java
@@ -25,7 +25,7 @@ public class Startup  implements ShellProcessListener {
   private static int LOG_DEFAULT = 0;
   private static int LOG_DISPLAY = 1;
 
-  public static int taskbar_height = 48;
+  public static int taskbar_height = 62;
 
   private static void load_config() {
     props = Linux.getJavaForceProperties();
