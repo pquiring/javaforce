d5aabf8a6776402936aa895f8ad0b13a06d8b0ce
Peter Quiring <pquiring@gmail.com>
2026-09-11 13:10:12 -0400

    add WLGlobal

---
+++ src/javaforce/linux/wl/WLGlobal.java
@@ -0,0 +1,12 @@
+package javaforce.linux.wl;
+
+/** Wayland Global reference.
+ *
+ * @author pquiring
+ */
+
+public class WLGlobal {
+  public int name;
+  public String iface;
+  public int ver;
+}
