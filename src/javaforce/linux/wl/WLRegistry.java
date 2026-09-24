a730061b89ba74432aee776e82e74cf354afb0fd
Peter Quiring <pquiring@gmail.com>
2026-09-24 09:11:01 -0400

    WLRegistry : fix bind() sign

--- src/javaforce/linux/wl/WLRegistry.java
+++ src/javaforce/linux/wl/WLRegistry.java
@@ -16,7 +16,7 @@ public class WLRegistry extends WLObject {
     Class cls = getClass();
     try {
       requests = new Method[] {
-        cls.getMethod("bind", new Class[] {int.class, int.class}),
+        cls.getMethod("bind", new Class[] {int.class, String.class, int.class, int.class}),
       };
       events = new Method[] {
         cls.getMethod("global", new Class[] {int.class, String.class, int.class}),
